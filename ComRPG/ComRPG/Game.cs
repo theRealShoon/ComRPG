6fc676cdfcb2546fc92e4d5b644137aede7a3596
theRealShoon <qs.tssean@gmail.com>
2020-09-16 18:34:51 -0400

    Added the Class "Game"

---
+++ ComRPG/ComRPG/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComRPG
+{
+    class Game
+    {
+        public void Start()
+        { 
+        
+        }
+    }
+}
--- ComRPG/ComRPG/Program.cs
+++ ComRPG/ComRPG/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ComRPG
 {
@@ -6,7 +7,7 @@ namespace ComRPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game game = new Game();
         }
     }
 }
