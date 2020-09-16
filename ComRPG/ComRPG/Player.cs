dd743ce5dda0f3cb283ec85524550c2176e346eb
theRealShoon <qs.tssean@gmail.com>
2020-09-16 19:51:26 -0400

    Player armor

--- ComRPG/ComRPG/Player.cs
+++ ComRPG/ComRPG/Player.cs
@@ -9,6 +9,17 @@ namespace ComRPG
     class Player
     {
         public string name { get; set; } // Stores the name for the player
+
+        //Armor
         Helmet helmet { get; set; }
+        Amulet amulet { get; set; }
+        Chestplate chestplate { get; set; }
+        Gloves gloves { get; set; }
+        Ring ringOne { get; set; }
+        Ring ringTwo { get; set; }
+        Leggings leggings { get; set; }
+        Boots boots { get; set; }
+
+        //Weapon
     }
 }
