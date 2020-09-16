2e1c2c8f21b7e403281926b953bb98d6bdc9188b
theRealShoon <qs.tssean@gmail.com>
2020-09-16 18:51:07 -0400

    Started working on implementing items

--- ComRPG/ComRPG/Items/ItemList.cs
+++ ComRPG/ComRPG/Items/ItemList.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ComRPG
+namespace ComRPG.Items
 {
     class ItemList
     {
--- ComRPG/ComRPG/Player.cs
+++ ComRPG/ComRPG/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ComRPG.Items;
 
 namespace ComRPG
 {
